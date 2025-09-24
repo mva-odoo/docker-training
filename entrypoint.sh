b09061b85e88821ea0b2cc68e7d79b2c158223fa
mva-odoo <mva@odoo.com>
2025-09-24 23:28:09 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 set -e
 
-# Démarrer PostgreSQL en mode foreground
 DATA_DIR="/var/lib/postgresql/data"
 
-echo "Démarrage de PostgreSQL..."
+echo "==== Vérification PostgreSQL ===="
 if [ ! -s "$DATA_DIR/PG_VERSION" ]; then
-    echo "Initialisation du cluster PostgreSQL..."
+    echo ">>> Initialisation du cluster PostgreSQL..."
     su - postgres -c "/usr/lib/postgresql/14/bin/initdb -E UTF8 -D $DATA_DIR"
-    su - postgres -c "/usr/lib/postgresql/14/bin/pg_ctl -D $DATA_DIR -l $DATA_DIR/logfile start"
     su - postgres -c "createuser -d -R -S odoo"
-else
-    echo "Cluster PostgreSQL déjà initialisé, lancement..."
-    su - postgres -c "/usr/lib/postgresql/14/bin/pg_ctl -D $DATA_DIR -l $DATA_DIR/logfile start"
+    su - postgres -c "createdb -O odoo odoo || true"
 fi
 
-# Démarrer PostgreSQL
+echo "==== Démarrage PostgreSQL en foreground ===="
+su - postgres -c "/usr/lib/postgresql/14/bin/postgres -D $DATA_DIR" &
+
+echo "==== Démarrage de code-server ===="
 exec su - odoo -c "code-server --bind-addr 0.0.0.0:8080"
-# exec su - odoo -c "/bin/bash" 
